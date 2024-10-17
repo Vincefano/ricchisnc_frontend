8459f81ebd6d5c69fe13663a3cdbc82e908742b6
Vincenzo <vincenzo.cavataio1@gmail.com>
2024-10-17 03:49:55 +0200

    adds tsdoc for logout page

--- src/pages/Logout/Logout.tsx
+++ src/pages/Logout/Logout.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { PAGES } from "../../constants/pages";
 import { useNavigate } from "react-router-dom";
 
+/** Page that clears localStorage and redirects to login page */
 export const Logout = () => {
   const navigate = useNavigate();
 
